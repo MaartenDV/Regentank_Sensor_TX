2fca7593bff086b33202f9bc788f59e15dcb2c7c
mdv <mdv@velleman.eu>
2022-08-07 17:36:57 +0200

    V1.0.2 - fault & averaging
    - 5V PS always on (maybe fixed some sensor issues)
    - ultrasonic averaging
    - fault LED blinks
    - utrasonic init

--- Regentank_Sensor_TX.ino
+++ Regentank_Sensor_TX.ino
@@ -27,14 +27,16 @@
 #define PIN_ECHO          10
 #define PIN_25_LED        11
 #define PIN_EMPTY_LED     12
+#define FAULT_LED         13  //LED_BUILTIN
 
-#define VOLUME_MEASUREMENT_MILI_DELAY           5000//36000000    //=1h
-#define TEMPERATURE_MEASUREMENT_MILI_DELAY      60000       //1min
+#define VOLUME_MEASUREMENT_MILI_DELAY           1000//36000000    //=1h
+#define TEMPERATURE_MEASUREMENT_MILI_DELAY      10000       //1min
 #define LED_ANIMATION_MILI_DELAY                200         //0.200s
 
 #define PACKET_BYTE_LENGTH    5
 #define PACKET_REV            1
 #define TASK_DELAY_MS         3000
+#define NUM_BLINK_ERROR_LED_DISTANCE_FAULT    5      
 
 //calibration params
 #define A   1.1938
@@ -45,12 +47,18 @@
 #define SENSOR_HEIGHT_MM            1700    //height sensor above empty well
 #define ULTRASONIC_DEPTH_SAMPLES    32
 
+#define ULTRASONIC_MINIMUM_ACCEPTABLE   10
+#define ULTRASONIC_MAXIMUM_ACCEPTABLE   SENSOR_HEIGHT_MM
+
 #define LEVEL_10_PERCENT            10
 #define LEVEL_25_PERCENT            25  
 #define LEVEL_50_PERCENT            50
 #define LEVEL_75_PERCENT            75
 #define LEVEL_90_PERCENT            90
 #define LEVEL_100_PERCENT           100
+
+#define SUCCESSFUL  1
+#define FAILED      0
 
 
 
@@ -66,14 +74,17 @@ unsigned long temperature_task_millis = 0;
 unsigned long led_task_millis = 0;
 
 word ultrasonic_depth; //unsigned 16bit
+word ultrasonic_depth_samples[ULTRASONIC_DEPTH_SAMPLES];
+word average_ultrasonic_depth;
 word rain_water_volume_percent;
 int ds18b20_temperature; //signed 16 bit
 float battery_voltage;
 byte tx_packet[PACKET_BYTE_LENGTH];
 bool bool_led_blink;
+uint8_t error_flashes;
 
 //******* FUNCTION DISCIPTION *******
-void do_volume_measurement_task(void);
+bool do_volume_measurement_task(void);
 void do_temperature_measurement_task(void);
 void do_battery_voltage_measurement_task(void);
 void do_led_animation_task(void);
@@ -82,7 +93,10 @@ void ps_disable(void);
 void ps_enabled_after_mili_delay(unsigned long);
 void vbat_measure_enable(void);
 void vbat_measure_disable(void);
+void fault_led_on(void);
+void fault_led_off(void);
 word calculate_volume_percent(word, word, word, word);
+word calculate_average_ultrasonic_depth(word);
 
 
 
@@ -112,11 +126,28 @@ void setup() {
   pinMode(PIN_75_LED, OUTPUT);
   pinMode(PIN_FULL_LED, OUTPUT);
   pinMode(PIN_READ_BAT_V, OUTPUT);
+  pinMode(FAULT_LED, OUTPUT);
 
   volume_task_millis = millis();
   temperature_task_millis = millis();
   led_task_millis = millis();
   rain_water_volume_percent = 0;
+  error_flashes = 0;
+
+  ps_enable();    //enable the 5V power supply. Keep always on. It drives otherwise the 5V sensors insane.
+  fault_led_off();
+
+  delay(50);
+
+  for(uint8_t measurement_repeats = ULTRASONIC_DEPTH_SAMPLES; measurement_repeats > 0; measurement_repeats--){
+    while(!do_volume_measurement_task());
+    Serial.print("sample taken: ");
+    Serial.print((ULTRASONIC_DEPTH_SAMPLES + 1) - measurement_repeats); 
+    Serial.print("/");
+    Serial.println(ULTRASONIC_DEPTH_SAMPLES); 
+    delay(20);
+  }
+  Serial.println("INIT DONE!");
 }
 
 
@@ -150,25 +181,36 @@ void loop() {
 // ***************************************
 //             TASKS volume
 //****************************************
-void do_volume_measurement_task() {
-  ps_enabled_after_mili_delay(25);
+bool do_volume_measurement_task() {
   Serial.print("The Distance is : ");
   ultrasonic_depth = ultrasonicSensor.getDistance();
-  ultrasonic_depth = (A*ultrasonic_depth + B); 
-  Serial.print(ultrasonic_depth);
-  Serial.println(" mm");
-  Serial.print("The Volume is : ");
-  rain_water_volume_percent = calculate_volume_percent(SENSOR_HEIGHT_MM, ultrasonic_depth, MUD_THICKNESS_MM, OUTFLOW_HEIGHT_MM);
-  Serial.print(rain_water_volume_percent);
-  Serial.println(" %");
-  ps_disable();
+  ultrasonic_depth = (A*ultrasonic_depth + B);    //correct for measuring faults
+  // Check if reading was successful
+  if ((ultrasonic_depth < ULTRASONIC_MAXIMUM_ACCEPTABLE) && (ultrasonic_depth > ULTRASONIC_MINIMUM_ACCEPTABLE)) {
+    Serial.print(ultrasonic_depth);
+    Serial.println(" mm");  
+    average_ultrasonic_depth = calculate_average_ultrasonic_depth(ultrasonic_depth);
+    Serial.print("The Average is : ");
+    Serial.print(average_ultrasonic_depth);
+    Serial.println(" mm");
+    Serial.print("The Volume is : ");   
+    rain_water_volume_percent = calculate_volume_percent(SENSOR_HEIGHT_MM, average_ultrasonic_depth, MUD_THICKNESS_MM, OUTFLOW_HEIGHT_MM);
+    Serial.print(rain_water_volume_percent);
+    Serial.println(" %");
+    return(SUCCESSFUL);
+  } else {
+    error_flashes += NUM_BLINK_ERROR_LED_DISTANCE_FAULT;
+    Serial.print("measuring distance fault!");
+    Serial.println();
+    return(FAILED);  
+  }
 }
+
 
 // ***************************************
 //           TASKS temperature
 //****************************************
 void do_temperature_measurement_task() {
-  ps_enabled_after_mili_delay(25);
   unsigned long temp_measure_current_millis = millis();
   Serial.print("The Temperature is : ");
   sensors.requestTemperatures(); // Send the command to get temperatures
@@ -185,7 +227,6 @@ void do_temperature_measurement_task() {
   Serial.print("Time to receive one wire temp :");
   Serial.print(millis() - temp_measure_current_millis);
   Serial.println(" ms");
-  ps_disable();
 }
 
 // ***************************************
@@ -199,6 +240,16 @@ void do_led_animation_task() {
   digitalWrite(PIN_50_LED, (rain_water_volume_percent >= LEVEL_50_PERCENT));
   digitalWrite(PIN_75_LED, (rain_water_volume_percent >= LEVEL_75_PERCENT));
   digitalWrite(PIN_FULL_LED, (rain_water_volume_percent >= LEVEL_100_PERCENT));
+
+  //display if needed error codes here
+  if (error_flashes) {
+    if (bool_led_blink) {
+      fault_led_on();
+    } else {
+      fault_led_off();
+      error_flashes--;
+    }
+  }
 }
 
 
@@ -213,7 +264,6 @@ void do_battery_voltage_measurement_task() {
   Serial.print(battery_voltage, 2);
   Serial.println(" V");
   vbat_measure_disable();
-  ps_disable();
 }
 
 
@@ -249,10 +299,33 @@ void vbat_measure_disable() {
   digitalWrite(PIN_READ_BAT_V, LOW);
 }
 
+//basic function to light up the fault LED
+void fault_led_on() {
+  digitalWrite(FAULT_LED, HIGH);
+}
+
+//basic function to light out the fault LED
+void fault_led_off() {
+  digitalWrite(FAULT_LED, LOW);
+}
+
 //function to calculate the percent volume in the rain water tank
 word calculate_volume_percent(word sensor_h_mm, word distance_h_mm, word mud_h_mm, word outflow_h_mm) {
   //todo checks
   return ((sensor_h_mm - distance_h_mm) - mud_h_mm) / (outflow_h_mm / 100);
+}
+
+//function to calculate the everage measured depth
+word calculate_average_ultrasonic_depth(word last_sample) {
+  word average = 0;  
+  for (uint8_t i = (ULTRASONIC_DEPTH_SAMPLES - 1) ; i > 0; i--) {
+    ultrasonic_depth_samples[i] = ultrasonic_depth_samples[i - 1];  //shift values in array one place
+    average += ultrasonic_depth_samples[i];  //meanwhile, add all values together
+  }
+  ultrasonic_depth_samples[0] = (word) last_sample;
+  average += ultrasonic_depth_samples[0];  //add last value together
+  average /= ULTRASONIC_DEPTH_SAMPLES; //devide by number of values in array -> average! :-
+  return average;
 }
 
 
